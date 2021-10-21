71d0922a9768be57e9e526eacb812cfff02d8606
arpit044 <55794222+arpit044@users.noreply.github.com>
2021-10-21 23:19:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        console.log(scroll)
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    })
+
+    // slide-up-script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Competitive Coder", "Python Developer", "Machine Learning Developer", "AI Developer"],
+
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed2 = new Typed(".typing-2", {
+        strings: ["Web Developer", "Competitive Coder", "Python Developer", "Machine Learning Developer", "AI Developer"],
+
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+})
